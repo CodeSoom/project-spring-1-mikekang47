ad77caa79324602179e2f044d8958306bdeaac06
mikekang47 <gyeong0308@gmail.com>
2021-03-25 17:52:31 +0900

    movieApiController에javadoc 추가

--- app/src/main/java/mikekang47/spring/project/controllers/movieApiController.java
+++ app/src/main/java/mikekang47/spring/project/controllers/movieApiController.java
@@ -1,12 +1,18 @@
 package mikekang47.spring.project.controllers;
 
 import mikekang47.spring.project.dto.MoviesData;
-import mikekang47.spring.project.service.MoviesService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+/**
+ * 영화 검색과 관련된 HTTP 요청을 처리합니다.
+ */
 public class movieApiController {
-
+    /**
+     * 전달된 keyword와 관련된 영화를 반환합니다.
+     * @param keyword
+     * @return 
+     */
     @GetMapping("/api/v1/movies/{keyword}")
     public MoviesData getMovie(@PathVariable String keyword) {
         return null;
