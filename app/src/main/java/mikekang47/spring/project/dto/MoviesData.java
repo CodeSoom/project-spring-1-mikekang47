a64f2368cb59775a103087a6871e2b3a989907d7
mikekang47 <gyeong0308@gmail.com>
2021-03-25 17:54:03 +0900

    moivesData 클래스와moviesService 클래스 생성

---
+++ app/src/main/java/mikekang47/spring/project/dto/MoviesData.java
@@ -0,0 +1,4 @@
+package mikekang47.spring.project.dto;
+
+public class MoviesData {
+}
---
+++ app/src/main/java/mikekang47/spring/project/service/MoviesService.java
@@ -0,0 +1,11 @@
+package mikekang47.spring.project.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MoviesService {
+
+
+}
