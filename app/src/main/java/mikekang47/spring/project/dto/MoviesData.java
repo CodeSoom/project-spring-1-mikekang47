e9371d43ca762a08a9525082a301a4b9c8983e85
mikekang47 <gyeong0308@gmail.com>
2021-03-29 17:12:53 +0900

    api를 통해서 표시할 데이터를 수정 	#

--- app/src/main/java/mikekang47/spring/project/dto/MoviesData.java
+++ app/src/main/java/mikekang47/spring/project/dto/MoviesData.java
@@ -1,17 +1,35 @@
 package mikekang47.spring.project.dto;
 
-import lombok.Data;
+import com.github.dozermapper.core.Mapping;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MoviesData {
     private int display;
     private Item[] items;
 
     @Data
     private class Item {
+        @NotBlank
+        @Mapping("title")
         public String title;
-        public String link;
+
+        @NotBlank
+        @Mapping("pubData")
         public Data pubData;
+
+        @NotBlank
+        @Mapping("director")
         public String director;
+
+        @NotBlank
+        @Mapping("actor")
+        public String actor;
     }
 }
