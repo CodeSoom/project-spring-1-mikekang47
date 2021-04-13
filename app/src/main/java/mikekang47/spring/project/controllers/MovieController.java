03107d79d8c6b6f938ff5c05583daf53e6200cd6
mikekang47 <gyeong0308@gmail.com>
2021-04-13 18:47:01 +0900

    Clean all class and restart

--- app/src/main/java/mikekang47/spring/project/controllers/MovieController.java
+++ app/src/main/java/mikekang47/spring/project/controllers/MovieController.java
@@ -2,15 +2,8 @@ package mikekang47.spring.project.controllers;
 
 import mikekang47.spring.project.application.MovieService;
 import mikekang47.spring.project.models.Movie;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
-import java.util.Set;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 @RequestMapping("/v1/search/movie")
